b4708b60a8b1d933c5fe3adaf43508f326e5edc2
Kosmas Mackrogamvrakis <4059779+kosmasm@users.noreply.github.com>
2022-04-11 13:07:07 +0300

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -2,5 +2,5 @@
 [ -f kube-hosts ] || exit 1;
 for a in $(cat kube-hosts); do
   scp kube-install.sh root@${a}:/tmp
-  ssh root@${a} "chmod 755 /tmp/kube-install;/tmp/kube-install"
+  ssh root@${a} "chmod 755 /tmp/kube-install.sh;/tmp/kube-install.sh"
 done
